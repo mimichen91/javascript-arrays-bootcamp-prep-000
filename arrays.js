c0123f3ff3fae3e671c164cfc979ee427befcd3a
Mimi Chen <yangminchen91@gmail.com>
2017-09-21 00:01:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift("element")
+
 }
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars, candy) {
-  var chocolateBars =
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift("element")
 }
